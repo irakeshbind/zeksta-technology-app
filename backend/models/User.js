99d0fdd0b7161356783bbcc31407607ddb4b1a9c
irakeshbind <rakeshbind351998@gmail.com>
2026-02-02 12:40:52 +0530

    that is password  encypt convert into bcrypt

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-  id: { type: mongoose.Schema.Types.ObjectId },
+  // id: { type: mongoose.Schema.Types.ObjectId },
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
