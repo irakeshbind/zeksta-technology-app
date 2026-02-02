5715d4af193faecd657792ecfc7d650cf32fca8b
irakeshbind <rakeshbind351998@gmail.com>
2026-02-02 11:43:15 +0530

    it is in this project  create  new user

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -2,7 +2,23 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User.js");
 router.post("/sginup", (req, res) => {
-  
+  try { 
+    // create a new user
+    const newUser = new User({
+      id: { type: Schema.Types.ObjectId },
+      name: req.body.name,
+      email: req.body.name,
+      phone: req.body.name,
+      address: req.body.name,
+      password: req.body.name,
+    });
+    const userData = new save();
+    res.status(200).json({
+      data: userData,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 router.post("/login", (req, res) => {
